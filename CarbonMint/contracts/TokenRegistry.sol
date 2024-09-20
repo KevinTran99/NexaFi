7964c42a43cc13abe81ebde1316d085f3ff937c6
KevinTran99 <kevintrann99@gmail.com>
2024-09-20 21:45:35 +0200

    Add NFTMint and NFTBurn events

--- CarbonMint/contracts/TokenRegistry.sol
+++ CarbonMint/contracts/TokenRegistry.sol
@@ -12,6 +12,9 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract TokenRegistry is ERC1155, AccessControl {
     bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
 
+    event NFTMint (address indexed mintAddress, uint256 id, uint256 amount);
+    event NFTBurn (address indexed burnAddress, uint256 id, uint256 amount);
+
     constructor() ERC1155("https://example.com/metadata/{id}.json") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINT_ROLE, msg.sender);
@@ -19,11 +22,15 @@ contract TokenRegistry is ERC1155, AccessControl {
 
     function mint(address _recipient, uint256 _id, uint256 _amount) external onlyRole(MINT_ROLE) {
         _mint(_recipient, _id, _amount, "");
+
+        emit NFTMint(_recipient, _id, _amount);
     }
 
     function burn(address _holder, uint256 _id, uint256 _amount) external {
         require(msg.sender == _holder, "You can only burn your own tokens");
         _burn(_holder, _id, _amount);
+
+        emit NFTBurn(_holder, _id, _amount);
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
