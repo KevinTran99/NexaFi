ef439145121b94446e80e541f9da99bc6bcb964e
KevinTran99 <kevintrann99@gmail.com>
2024-09-27 18:43:18 +0200

    Set up project with basic navbar and layout structure using initial CSS

---
+++ NexaFi Frontend/nexafi/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NexaFi from './NexaFi';
+
+function App() {
+  return (
+    <div className="App">
+      <NexaFi></NexaFi>
+    </div>
+  );
+}
+
+export default App;
---
+++ NexaFi Frontend/nexafi/src/NexaFi.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NexaFi = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#main">Main</a>
+          </li>
+          <li>
+            <a href="#about">Vision</a>
+          </li>
+          <li>
+            <a href="#footer">Team</a>
+          </li>
+        </ul>
+      </nav>
+
+      <section id="main" className="main-section"></section>
+    </div>
+  );
+};
+
+export default NexaFi;
