0abe14ae85523f9bdd226327c19138ef892bdfab
KevinTran99 <kevintrann99@gmail.com>
2024-11-11 21:02:02 +0100

    Update import path to correct ReentrancyGuard location in utils

--- NexaFi/contracts/Marketplace.sol
+++ NexaFi/contracts/Marketplace.sol
@@ -9,7 +9,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title Marketplace
