67ba8701f5135bf2af8e7b6a4a806a0baa1c4f54
KevinTran99 <kevintrann99@gmail.com>
2024-12-03 16:09:08 +0100

    Add contract addresses

--- NexaFi Backend/src/config/config.mjs
+++ NexaFi Backend/src/config/config.mjs
@@ -5,5 +5,7 @@ export const config = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   ALCHEMY_KEY: process.env.ALCHEMY_KEY || '',
+  USDT_ADDRESS: process.env.USDT_ADDRESS || '',
+  TOKEN_REGISTRY_ADDRESS: process.env.TOKEN_REGISTRY_ADDRESS || '',
   MARKETPLACE_ADDRESS: process.env.MARKETPLACE_ADDRESS || '',
 };
